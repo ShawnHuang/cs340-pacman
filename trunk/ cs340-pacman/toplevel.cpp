27a9253aaaaf7d89744f4a212b4499120af9385a
usha.sanaga <usha.sanaga@c94e964c-9837-11de-bb56-53d3cd047523>
2009-10-14 01:19:22 +0000

    Added enemy to the Toplevel class and refactored to handle the scene in a better way and repositioned Topbar.

--- trunk/ cs340-pacman/toplevel.cpp
+++ trunk/ cs340-pacman/toplevel.cpp
@@ -1,37 +1,28 @@
 #include "toplevel.h"
-#include <QApplication>
-//#include "MyButtons.h"
-#include <QSplashScreen>
-#include <QtGui>
-#include "player.h"
-#include "dot.h"
-#include "maploader.h"
-#include "wall.h"
-#include "player.h"
-#include "enemy.h"
-#include "topbar.h"
-#include "dot.h"
-#include "bigdot.h"
-#include <windows.h>
-#include <vector>
 
-TopLevel :: TopLevel()
+TopLevel :: TopLevel(QGraphicsScene *scene) : QGraphicsView(scene)
 {
-    QGraphicsScene *scene = new QGraphicsScene(this);
-    TopBar *stat= new TopBar();
-    QPalette p;
-    p.setColor(QPalette::Background,Qt::magenta);
-    stat->setPalette(p);
-
-
-    scene->setSceneRect(0,0,550,550);
-    scene->setFocus();
-    setScene(scene);
+    // set the view properties
     setRenderHint(QPainter::Antialiasing);
     setBackgroundBrush(QColor(0,0,0,255));
     setCacheMode(QGraphicsView::CacheBackground);
     setViewportUpdateMode(QGraphicsView::BoundingRectViewportUpdate);
     setWindowTitle(QT_TRANSLATE_NOOP(QGraphicsView, "Pac-Man"));
+
+    // set the scene properties
+    scene->setSceneRect(0,-35,550,505);
+    scene->setFocus();
+
+
+    // create the scene elements
+    QPalette p;
+    p.setColor(QPalette::Background,Qt::magenta);
+
+    TopBar *stat= new TopBar();
+    stat->setPalette(p);
+    stat->setGeometry(0,-35,500,30);
+
+    scene->addWidget(stat);
 
     //Maploader entry point
     MapLoader *mp = new MapLoader;
@@ -44,39 +35,46 @@ TopLevel :: TopLevel()
         msg->showMessage("List size zero in map. Please check the file.");
     }
 
-        Dot *allDots = new Dot[mp->dotmap.size()];
-        Wall *wallblock = new Wall[mp->map.size()];
-        BigDot *powerDots = new BigDot[mp->powerdotmap.size()];
+    Dot *allDots = new Dot[mp->dotmap.size()];
+    Wall *wallblock = new Wall[mp->map.size()];
+    BigDot *powerDots = new BigDot[mp->powerdotmap.size()];
 
-
-     int i =0;
-     for(QMap<QString, CoordChar>::const_iterator it = mp->powerdotmap.constBegin(); it != mp->powerdotmap.constEnd(); it++)
+    int i =0;
+    for(QMap<QString, CoordChar>::const_iterator it = mp->powerdotmap.constBegin(); it != mp->powerdotmap.constEnd(); it++)
     {
          powerDots[i].setPos(it.value().xcoord*WIDTH,it.value().ycoord*HEIGHT);
          scene->addItem(&powerDots[i]);
          i++;
     }
 
-
-      i =0;
-     for(QMap<QString, CoordChar>::const_iterator it = mp->dotmap.constBegin(); it != mp->dotmap.constEnd(); it++)
+    i =0;
+    for(QMap<QString, CoordChar>::const_iterator it = mp->dotmap.constBegin(); it != mp->dotmap.constEnd(); it++)
     {
          allDots[i].setPos(it.value().xcoord*WIDTH,it.value().ycoord*HEIGHT);
          scene->addItem(&allDots[i]);
          i++;
     }
 
-     i=0;
-     for(QMap<QString, CoordChar>::const_iterator it = mp->map.constBegin(); it != mp->map.constEnd(); it++)
+    i=0;
+    for(QMap<QString, CoordChar>::const_iterator it = mp->map.constBegin(); it != mp->map.constEnd(); it++)
     {
          wallblock[i].setPos(it.value().xcoord*WIDTH,it.value().ycoord*HEIGHT);
          scene->addItem(&wallblock[i]);
          i++;
     }
 
-     scene->addWidget(stat);
-     stat->setGeometry(0,-20,500,30);
+     //create an enemy
+     Enemy *e = new Enemy(1,1,Character::DIR_DOWN,mp);
+     scene->addItem(e);
 
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), scene, SLOT(advance()));
 }
+
+void TopLevel::start(int msec)
+{
+    timer->start(msec);
+}
+
 
 
