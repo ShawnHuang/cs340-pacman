029536bf62befd59c60b5c02b4245faa99e0a56c
rchoud2 <rchoud2@c94e964c-9837-11de-bb56-53d3cd047523>
2009-10-15 18:14:31 +0000

    Added all my code, display screen shows up but still working on connecting it using manager class.

--- trunk/ cs340-pacman/main.cpp
+++ trunk/ cs340-pacman/main.cpp
@@ -1,15 +1,17 @@
 
 #include <QGraphicsScene>/*****************************************************
   File Name: main.cpp
-  Description: Creates, loads the scene and add items
-  Created by: Riddhi Kapasi
-  Modified by:
+  Description: makes the objects of the scene classes and show the scenes
+  Created by:  Riddhi Kapasi
+  Modified by: Rafe Choudhry
 ******************************************************/
 
 #include <QApplication>
 #include "toplevel.h"
 //#include <windows.h>
 #include <vector>
+#include "scenes.h"
+#include "displayMap.h"
 
 
 
@@ -22,10 +24,23 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     QGraphicsScene *scene = new QGraphicsScene();
-    TopLevel display(scene);
-    display.show();
-    display.start(100);
+
+
+    displayMap *map = new displayMap;
+    //map->hide();
+    //map->show();
+
+
+
+    //DISPLAYING SCREEN(will be transfered to the other class)
+    QGraphicsScene *scene1 = new QGraphicsScene();
+    scenes display1(scene1);
+    display1.setBackgroundBrush(QPixmap("C:/Users/Sohaib/Desktop/images/display.gif"));
+    display1.resize(640,480);
+    display1.show();
 
     return app.exec();
+
+    //making a maneger class who will
 
 }
