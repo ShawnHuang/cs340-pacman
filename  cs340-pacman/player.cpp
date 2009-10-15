1595df690809d95fafc2028747fb51195d3930a9
riddhi.kapasi <riddhi.kapasi@c94e964c-9837-11de-bb56-53d3cd047523>
2009-10-15 18:06:41 +0000

    updating player character

---  cs340-pacman/player.cpp
+++  cs340-pacman/player.cpp
@@ -1,28 +1,40 @@
 #include "player.h"
-
+#include <Qt>
 Player::Player()
 {
+
 }
 
-Player::Player(int x,int y, int dir) : Character::Character(x,y,dir)
+Player::Player(int x,int y, QGraphicsScene *scene) : QGraphicsItem(0, scene)
 {
+    xCoord = x;
+    yCoord = y;
+    setFlag(ItemIsFocusable);
+
+     actionmap.insert(Qt::Key_Up, Up);
+     actionmap.insert(Qt::Key_Down, Down);
+     actionmap.insert(Qt::Key_Right, Right);
+     actionmap.insert(Qt::Key_Left, Left);
+     setFocus();
+     setPos(260, 300);
+
 }
 
 int Player::getTYpe()
 {
-    return TYPE_PLAYER;
+    return 0;
 }
 
 void Player::paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *)
 {
    painter->setBrush(Qt::yellow);
    painter->setPen(Qt::yellow);
-   painter->drawEllipse(0,0,30,30);
+   painter->drawEllipse(xCoord,yCoord,30,30);
 }
 
 QRectF Player::boundingRect() const
 {
-    return QRectF(0,0,30,30);
+    return QRectF(xCoord,yCoord,30,30);
 }
 
 void Player::setAndAddStates()
@@ -45,10 +57,34 @@ void Player::update()
 
 }
 
+void Player::keyPressEvent(QKeyEvent *event)
+{
+    Action a = actionmap.value(event->key());
 
+     switch (a)
+     {
+         case Right:
+            xCoord += 1;
+            break;
 
+          case Left:
+            xCoord -= 1;
+            break;
 
+          case Down:
+            yCoord += 1;
+            break;
 
+          case Up:
+            yCoord -= 1;
+            break;
+
+          default:
+            break;
+     }
+     this->setPos(xCoord, yCoord);
+     //event->accept();
+}
 
 
 
